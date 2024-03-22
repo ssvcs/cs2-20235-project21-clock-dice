503c0c9e1c7f3edc213b306f38485d088b34c6e7
ssvcs <ssvcs@rit.edu>
2024-03-22 10:56:55 -0400

    Updated run configs.

--- src/puzzles/dice/Dice.java
+++ src/puzzles/dice/Dice.java
@@ -4,8 +4,8 @@ import puzzles.common.solver.Solver;
 
 public class Dice {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.out.println("Usage: java Dice dice-files start end");
+        if (args.length < 3) {
+            System.out.println("Usage: java Dice start end die1 die2...");
         } else {
             // TODO
         }
